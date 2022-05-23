f1286b08275ce35f96dd479062f27d40b9411d0c
Arjun Krishna <arjunkrishna1306@gmail.com>
2022-05-23 12:28:41 -0400

    update the home page to talk about the book club

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `University of Waterloo Book Club`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.uwbookclub.github.io`,
   },
   plugins: [],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,8 +57,7 @@ const descriptionStyle = {
 }
 
 const docLink = {
-  text: "TypeScript Documentation",
-  url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
+  text: "Books we're curently reading:",
   color: "#8954A8",
 }
 
@@ -81,47 +80,25 @@ const badgeStyle = {
 // data
 const links = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    text: "The Dispossessed",
+    url: "https://www.goodreads.com/book/show/13651.The_Dispossessed",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "A tall alien from a communist planet visits a capitalist planet to open up their cultures to each other. Anarchy and hijinks ensue!",
     color: "#E95800",
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
+    text: "Dance of The Reptiles",
+    url: "https://www.goodreads.com/book/show/17978478-dance-of-the-reptiles",
     description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+      "Stranger than fact stories about animal welfare, polluted rivers, and the broken criminal justice system in Florida.",
+    color: "#E95800",
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
+    text: "The Count of Monte Cristo",
+    url: "https://www.goodreads.com/book/show/7126.The_Count_of_Monte_Cristo?ac=1&from_search=true&qid=eVwGW707Ih&rank=1",
     description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
+      "Everything is going well for Dantes until he is made the fall guy for a Bonapartist conspiracy to protect some powerful people. He comes back decades later with billions and shows them who's boss.",
+    color: "#E95800",
   },
 ]
 
@@ -129,25 +106,23 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
+      <title>UW Book Club</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Welcome!
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        🎉🎉🎉
+        <span style={headingAccentStyles}>— this is the UW Book Club </span>
+        📚 🤓
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
+        We meet weekly to talk about the books we're reading and hang out.
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
-          <a
+          <h1
             style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
           >
             {docLink.text}
-          </a>
+          </h1>
         </li>
         {links.map(link => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
@@ -168,10 +143,6 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
     </main>
   )
 }
