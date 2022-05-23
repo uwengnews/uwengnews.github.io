d585761f33685cfa8e3d56ca93813a8337e4d7c8
Arjun Krishna <arjunkrishna1306@gmail.com>
2022-05-23 12:00:12 -0400

    add hello world gatsby site

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `University of Waterloo Book Club`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
