9d15a230d3e34bb3b09b72d75d1fd7c05d5014f5
Arjun Krishna <arjunkrishna1306@gmail.com>
2022-09-19 11:16:37 -0400

    add first issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import * as React from "react"
+import issue from "../issues/issue.pdf";
 
 // styles
 const pageStyles = {
   color: "#232129",
-  padding: 96,
+  padding: 32,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
+  marginBottom: 5,
   maxWidth: 320,
 }
 const headingAccentStyles = {
@@ -73,8 +74,26 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
+const iframeStyle = {
+  width: '100%',
+  height: 750,
+
+}
+
+const gridContainer = {
+  width: '100%',
+  overflow: 'hidden',
+}
+
+const gridElem = {
+  width: '45%',
+  float: 'left',
+  padding: 20,
+}
+
 const githubIssuesLink = "https://github.com/uwengnews/uwengnews.github.io/issues";
 const articleSubmissionLink = "https://forms.gle/QMmLBQv6YemSdCfC6";
+const coverImageSubmissionLInk = "https://docs.google.com/forms/d/e/1FAIpQLSdUmb3xwXgFfQCb-2lgpZLQbOgPh1n7e3_Eh_XIywF8C5WLxQ/viewform";
 
 // markup
 const IndexPage = () => {
@@ -84,16 +103,24 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         ENGnews
       </h1>
-      <h3>UWaterloo's avant-garde, stream-of-conciousness engineering student paper</h3>
-      <p style={paragraphStyles}>
-        We're starting in the Fall term of 2022, first issue out on September 12
-      </p>
-      <p style={paragraphStyles}>
-        We will finally break the chokehold mathNEWS has on how nerds get high quality information on the University of Waterloo campus
-      </p>
-      <p style={paragraphStyles}>
-        Have ideas or want to join? Submit articles <a href={articleSubmissionLink}>here</a> or create a <a href={githubIssuesLink}>github issue</a>
-      </p>
+      <div style={gridContainer}>
+        <div style={gridElem}>
+          <h2>Issue 0000 0001</h2>
+          <iframe id="issuePdf" title="Issue 0000 0001" src={issue} style={iframeStyle}></iframe>
+        </div>
+        <div style={gridElem}>
+          <h3>The Avant-Garde Stream-of-Conciousness Laurieh Engeneerneng Studet Paper</h3>
+          <p style={paragraphStyles}>
+            Approved by Lourier Engeneering.
+          </p>
+          <p style={paragraphStyles}>
+            Pick up a physical copy in PAC, SLC, QNC, RCH or E5. Next issue out on October 3rd.
+          </p>
+          <p style={paragraphStyles}>
+            Have ideas or want to join? Submit articles <a href={articleSubmissionLink}>here</a>, cover images <a href={articleSubmissionLink}>here</a> or create a <a href={githubIssuesLink}>github issue</a>
+          </p>
+        </div>
+      </div>
     </main>
   )
 }
